fd9034abf593562f284e03d597fb0064f28b5d72
anemone <annamatskevich866@gmail.com>
2024-07-19 00:22:41 +0700

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI, Request
-from fastapi.responses import HTMLResponse
+from fastapi.responses import HTMLResponse, RedirectResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from geopy.adapters import AioHTTPAdapter
@@ -7,20 +7,26 @@ from geopy.geocoders import Nominatim
 import httpx
 
 app = FastAPI()
-app.mount("/static", StaticFiles(directory="static"), name="static")
+# app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
 
+@app.get("/", response_class=HTMLResponse)
+async def home(request: Request):
+    return templates.TemplateResponse("base.html", {"request": request})
 
-# @app.get("/", response_class=HTMLResponse)
-# async def home(request: Request):
-#     return templates.TemplateResponse("base.html", {"request": request})
-url = "https://api.open-meteo.com/v1/forecast"
+@app.get("/weather")
+async def get_weather(city: str = None):
+    # Здесь вы можете добавить логику для получения широты и долготы из строки поиска с использованием вашего метода или API геокодирования
+    async with Nominatim(user_agent="WebWeatherForecastApp", adapter_factory=AioHTTPAdapter,) as geolocator:
+        location = await geolocator.geocode(city)
+        if location:
+            latitude = location.latitude
+            longitude = location.longitude
 
-
-@app.get("/weather", response_class=HTMLResponse)
-async def get_weather(city: str):
-    async with httpx.AsyncClient() as client:
-        params = {"latitude": 40.7143, "longitude": -74.006, "hourly": "temperature_2m"}
-        response = await client.get()
-        weather_data = response.json()
-        return {"city": city, "weather": weather_data}
+            url = f"https://api.open-meteo.com/v1/forecast?latitude={latitude}&longitude={longitude}&hourly=temperature_2m"
+            async with httpx.AsyncClient() as client:
+                    response = await client.get(url)
+                    return {"city": city, "weather": response.json()}
+        else:
+            # return {"error": "Геоданные не найдены для указанного города"}
+            return RedirectResponse(url="/")
